398c96cfe45d4501fd86615580911725281aab3d
zhongzc <zhongzc_arch@outlook.com>
2020-06-17 17:44:16 +0800

    per thread
    Signed-off-by: zhongzc <zhongzc_arch@outlook.com>

--- benches/trace.rs
+++ benches/trace.rs
@@ -61,5 +61,39 @@ fn trace_deep_bench(c: &mut Criterion) {
     );
 }
 
-criterion_group!(benches, trace_wide_bench, trace_deep_bench);
+fn trace_multi_thread(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "per_thread_collect",
+        |b, thread_cnt| {
+            b.iter(|| {
+                let (root, collector) = minitrace::trace_enable(0u32);
+
+                let mut threads = Vec::with_capacity(*thread_cnt);
+
+                for i in 0..*thread_cnt {
+                    let handle = minitrace::trace_crossthread(i as u32);
+                    let join_handle = std::thread::spawn(move || {
+                        let mut handle = handle;
+                        let _g = handle.trace_enable();
+
+                        for i in 0..1000 {
+                            let _g = minitrace::new_span(i as u32);
+                        }
+                    });
+                    threads.push(join_handle);
+                }
+
+                for handle in threads {
+                    let _ = handle.join();
+                }
+
+                drop(root);
+                let _span_sets = collector.collect();
+            });
+        },
+        vec![4, 8, 16, 32],
+    );
+}
+
+criterion_group!(benches, trace_wide_bench, trace_deep_bench, trace_multi_thread);
 criterion_main!(benches);
